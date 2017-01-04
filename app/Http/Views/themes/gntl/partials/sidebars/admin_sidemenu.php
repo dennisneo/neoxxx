4d84147b23019944d36ce6ec0fd07515029fe1c5
Dennis Agulo <dennisagulo@gmail.com>
2017-01-04 08:38:50 +0800

    admin icon changes

--- app/Http/Views/themes/gntl/partials/sidebars/admin_sidemenu.php
+++ app/Http/Views/themes/gntl/partials/sidebars/admin_sidemenu.php
@@ -1,11 +1,11 @@
 <ul class="nav side-menu">
     <li><a href="<?php echo Url('admin/dashboard') ?>"><i class="fa fa-home"></i> <b>Dashboard</b> <span class="fa fa-chevron-down"></span></a></li>
-    <li><a href="<?php echo Url('admin/schedules') ?>"><i class="fa fa-home"></i> <b>Class Schedules</b> <span class="fa fa-chevron-down"></span></a></li>
-    <li><a href="<?php echo Url('admin/applicants') ?>"><i class="fa fa-table"></i> <b>Applicants</b> <span class="fa fa-chevron-down"></span></a></li>
-    <li><a href="<?php echo Url('admin/teachers') ?>"><i class="fa fa-table"></i> <b>Teachers</b> <span class="fa fa-chevron-down"></span></a></li>
-    <li><a href="<?php echo Url('admin/students') ?>"><i class="fa fa-table"></i> <b>Students</b> <span class="fa fa-chevron-down"></span></a></li>
-    <li><a href="<?php echo Url('admin/learning-goals') ?>"><i class="fa fa-table"></i> <b>Learning Goals</b> <span class="fa fa-chevron-down"></span></a></li>
-    <li><a href="<?php echo Url('admin/pe') ?>"><i class="fa fa-table"></i> <b>Placement Exam</b> <span class="fa fa-chevron-down"></span></a></li>
+    <li><a href="<?php echo Url('admin/schedules') ?>"><i class="fa fa-calendar"></i> <b>Class Schedules</b> <span class="fa fa-chevron-down"></span></a></li>
+    <li><a href="<?php echo Url('admin/applicants') ?>"><i class="fa fa-users"></i> <b>Applicants</b> <span class="fa fa-chevron-down"></span></a></li>
+    <li><a href="<?php echo Url('admin/teachers') ?>"><i class="fa fa-users"></i> <b>Teachers</b> <span class="fa fa-chevron-down"></span></a></li>
+    <li><a href="<?php echo Url('admin/students') ?>"><i class="fa fa-users"></i> <b>Students</b> <span class="fa fa-chevron-down"></span></a></li>
+    <li><a href="<?php echo Url('admin/learning-goals') ?>"><i class="fa fa-check"></i> <b>Learning Goals</b> <span class="fa fa-chevron-down"></span></a></li>
+    <li><a href="<?php echo Url('admin/pe') ?>"><i class="fa fa-pencil"></i> <b>Placement Exam</b> <span class="fa fa-chevron-down"></span></a></li>
     <li><a href="<?php echo Url('admin/records') ?>"><i class="fa fa-table"></i> <b>Performance Records</b> <span class="fa fa-chevron-down"></span></a></li>
     <li><a href="<?php echo Url('admin/settings') ?>"><i class="fa fa-gear"></i> <b>Settings</b> <span class="fa fa-chevron-down"></span></a></li>
 </ul>
